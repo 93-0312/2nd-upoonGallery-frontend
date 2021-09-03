fd2fb06410d31ff5b6a0f03b99424aabd92d5220
lee5992 <dlwogus432@gmail.com>
2021-09-03 23:43:59 +0900

    ?

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -11,13 +11,13 @@ export default function Main() {
         UPoon Gallery
       </Title>
       <Desc>
-        <DescTitle>그림으로 변화된 공간</DescTitle>
+        <DescTitle>그림으로 변화된 공간</DescTitle>
         <DescText>
-          유명 작가들의 하나뿐인 그림들을 합리적인 가격으로대여하고,
+          유명 작가들의 하나뿐인 그림들을 합리적인 가격으로 대여하고,
         </DescText>
         <DescText>
           3개월 마다 언제든 원하는 그림으로 교체할 수 있는 서비스를
-          이용해보세요.
+          이용해보세요.
         </DescText>
       </Desc>
       <MiddleBanner />
